ed7dfb0db7fa0640bbbb62b90a3dfe414de16cab
jongmine <jongmine.ch@gmail.com>
2026-03-31 01:08:26 +0900

    Setup for rate-limiter.

---
+++ rate-limiter/src/main/java/me/jongmine/ratelimiter/RateLimiterApplication.java
@@ -0,0 +1,13 @@
+package me.jongmine.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RateLimiterApplication.class, args);
+	}
+
+}
