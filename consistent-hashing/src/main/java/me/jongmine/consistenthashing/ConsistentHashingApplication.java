b7dc02d2919f60c95bdcdaeb2790bf54775e256b
jongmine <jongmine.ch@gmail.com>
2026-03-31 01:13:34 +0900

    Setup for consistent-hashing.

---
+++ consistent-hashing/src/main/java/me/jongmine/consistenthashing/ConsistentHashingApplication.java
@@ -0,0 +1,13 @@
+package me.jongmine.consistenthashing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsistentHashingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ConsistentHashingApplication.class, args);
+	}
+
+}
---
+++ consistent-hashing/src/test/java/me/jongmine/consistenthashing/ConsistentHashingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jongmine.consistenthashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistentHashingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
